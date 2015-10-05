518597b89a775587e5eee6fdd74951750a99136f
Magnus Nordlander <magnus@nordlander.se>
2015-10-05 13:42:10 +0200

    Updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ xcode-select --install
 sudo easy_install pip
 sudo pip install ansible
 ansible-galaxy install -r requirements.yml
-ansible-playbook desktop.yml
+ansible-playbook desktop.yml --ask-become-pass
